85f7e61c7ce9b5c6fe9edc5aae8f9bfc9d7c1e55
Sarah Zimmerman <143110222+STayZim@users.noreply.github.com>
2024-04-16 08:06:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -100,6 +100,6 @@ app.get('/account/all', function (req, res) {
     });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.listen(port);
 console.log('Running on port: ' + port);
